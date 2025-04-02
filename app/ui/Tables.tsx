6fb28e91c71bedd98d0161c972f8a8fe3400409d
Cristian <ctapia.macaya@gmail.com>
2025-04-02 14:54:25 -0300

    Block view working with diferent viewports

--- app/ui/Tables.tsx
+++ app/ui/Tables.tsx
@@ -8,16 +8,17 @@ export default function Tables() {
           <button className="bg-red-500 border-1 p-2">Agregar Mesa</button>
           <input className="border-1  p-2" type="search" placeholder="Buscar Mesa" />
         </div>
-        <div className="flex">
+
+        <div className="flex flex-wrap gap-4 justify-center p-4">
           {tablesArray.map((option) => (
-            <div key={option.id} className="box-border size-46 border-1 p-4 rounded">
-              <div className="flex">
-                <div className="w-32 flex">Mesa {option.id}</div>
-                <button>O</button>
+            <div key={option.id} className="box-border border p-4 rounded shadow-md bg-gray w-[200px]">
+              <div className="flex justify-between items-center">
+                <div className="w-32">Mesa {option.id}</div>
+                <button className="bg-red-500 text-white px-2 py-1 rounded">O</button>
               </div>
-              <div className="flex justify-center gap-2 p-10">
-                <div className="p-2 box-border border-1">Atención</div>
-                <div className="p-2 box-border border-1">Cuenta</div>
+              <div className="flex justify-center gap-2 p-4">
+                <div className="p-2 box-border border rounded">Atención</div>
+                <div className="p-2 box-border border rounded">Cuenta</div>
               </div>
             </div>
           ))}
