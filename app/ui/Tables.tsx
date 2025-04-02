af4a65288fe07a8df721abb7359447248b82bd4d
Cristian <ctapia.macaya@gmail.com>
2025-04-02 15:11:14 -0300

    Improving alignment on the boxes (block view)

--- app/ui/Tables.tsx
+++ app/ui/Tables.tsx
@@ -4,19 +4,19 @@ export default function Tables() {
   return (
     <>
       <div className="flex flex-col">
-        <div className="text-white flex items-center gap-4 pb-4">
+        <div className="text-white flex items-center gap-4 pb-8">
           <button className="bg-red-500 border-1 p-2">Agregar Mesa</button>
           <input className="border-1  p-2" type="search" placeholder="Buscar Mesa" />
         </div>
 
-        <div className="flex flex-wrap gap-4 justify-center p-4">
+        <div className="flex flex-wrap gap-8 justify-center">
           {tablesArray.map((option) => (
             <div key={option.id} className="box-border border p-4 rounded shadow-md bg-gray w-[200px]">
               <div className="flex justify-between items-center">
                 <div className="w-32">Mesa {option.id}</div>
-                <button className="bg-red-500 text-white px-2 py-1 rounded">O</button>
+                <button className="bg-red-500 text-white p-2 py-1 rounded">E</button>
               </div>
-              <div className="flex justify-center gap-2 p-4">
+              <div className="flex justify-center gap-4 p-4">
                 <div className="p-2 box-border border rounded">Atención</div>
                 <div className="p-2 box-border border rounded">Cuenta</div>
               </div>
