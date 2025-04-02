8ffac3a6b98eeceddbb6829c573c0057ef8891ef
Cristian <ctapia.macaya@gmail.com>
2025-04-02 14:42:44 -0300

    Adding structure to the block view

--- app/ui/Tables.tsx
+++ app/ui/Tables.tsx
@@ -3,25 +3,26 @@ import { tablesArray } from "../lib/data";
 export default function Tables() {
   return (
     <>
-      {/* <div className="grid grid-rows-[10%, 90%]"> */}
-      <header className="bg-green-800 text-white flex items-center">
-        <div className="border-1  p-2">+</div>
-        <input className="border-1  p-2" type="search" />
-      </header>
-
-      {tablesArray.map((option) => (
-        <div key={option.id} className="box-border size-40 border-1 p-4 rounded">
-          <div className="flex">
-            <div className="w-32 flex">Mesa {option.id}</div>
-            <button>O</button>
-          </div>
-          <div className="flex justify-center gap-2 p-8">
-            <div className="p-2 box-border border-1">Atención</div>
-            <div className="p-2 box-border border-1">Cuenta</div>
-          </div>
+      <div className="flex flex-col">
+        <div className="text-white flex items-center gap-4 pb-4">
+          <button className="bg-red-500 border-1 p-2">Agregar Mesa</button>
+          <input className="border-1  p-2" type="search" placeholder="Buscar Mesa" />
         </div>
-      ))}
-      {/* </div> */}
+        <div className="flex">
+          {tablesArray.map((option) => (
+            <div key={option.id} className="box-border size-46 border-1 p-4 rounded">
+              <div className="flex">
+                <div className="w-32 flex">Mesa {option.id}</div>
+                <button>O</button>
+              </div>
+              <div className="flex justify-center gap-2 p-10">
+                <div className="p-2 box-border border-1">Atención</div>
+                <div className="p-2 box-border border-1">Cuenta</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
