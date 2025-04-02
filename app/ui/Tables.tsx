4dc813c12f3bb7ea032a49cd37595ef9e0e7eb10
Cristian <ctapia.macaya@gmail.com>
2025-04-02 14:19:05 -0300

    Block view partially working

--- app/ui/Tables.tsx
+++ app/ui/Tables.tsx
@@ -3,15 +3,25 @@ import { tablesArray } from "../lib/data";
 export default function Tables() {
   return (
     <>
+      {/* <div className="grid grid-rows-[10%, 90%]"> */}
+      <header className="bg-green-800 text-white flex items-center">
+        <div className="border-1  p-2">+</div>
+        <input className="border-1  p-2" type="search" />
+      </header>
+
       {tablesArray.map((option) => (
-        <div key={option.id} className="box-border size-40 border-1 p-4 justify-center">
-          <div>Mesa {option.id}</div>
-          <div>
-            <button>Atención</button>
-            <button>Cuenta</button>
+        <div key={option.id} className="box-border size-40 border-1 p-4 rounded">
+          <div className="flex">
+            <div className="w-32 flex">Mesa {option.id}</div>
+            <button>O</button>
+          </div>
+          <div className="flex justify-center gap-2 p-8">
+            <div className="p-2 box-border border-1">Atención</div>
+            <div className="p-2 box-border border-1">Cuenta</div>
           </div>
         </div>
       ))}
+      {/* </div> */}
     </>
   );
 }
