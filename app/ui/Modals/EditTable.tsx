df5d7345e471dbdc99e1c90a2640a4bdbdab1b27
Cristian <ctapia.macaya@gmail.com>
2025-04-12 23:18:29 -0400

    Improving input numbers to permit only numbers and no other math operational symbol/number

--- app/ui/Modals/EditTable.tsx
+++ app/ui/Modals/EditTable.tsx
@@ -7,6 +7,12 @@ export default function EditTable() {
           <input
             type="number"
             className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+            // placeholder="Opcional"
+            onKeyDown={(e) => {
+              if (["e", "E", "+", "-"].includes(e.key)) {
+                e.preventDefault();
+              }
+            }}
           />
         </div>
       </div>
