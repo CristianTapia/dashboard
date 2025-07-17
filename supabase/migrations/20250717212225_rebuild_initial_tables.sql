055513a4942242c3948df465bfdd0adbd83a298d
Cristian <ctapia.macaya@gmail.com>
2025-07-17 17:53:00 -0400

    Creating db tables with Supabase migrations

---
+++ supabase/migrations/20250717212225_rebuild_initial_tables.sql
@@ -0,0 +1,15 @@
+-- (Re)crear categories y products
+CREATE TABLE IF NOT EXISTS categories (
+  id         BIGSERIAL PRIMARY KEY,
+  name       TEXT NOT NULL UNIQUE,
+  created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id           BIGSERIAL PRIMARY KEY,
+  name         TEXT NOT NULL,
+  price        INTEGER NOT NULL,
+  stock        INTEGER NOT NULL,
+  category_id  BIGINT REFERENCES categories(id) ON DELETE SET NULL,
+  created_at   TIMESTAMPTZ DEFAULT now()
+);
