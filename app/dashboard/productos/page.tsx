bc197975ac890201f21383c09d4e31c8940b79ff
Cristian <ctapia.macaya@gmail.com>
2025-08-04 22:21:19 -0400

    Replacing helpers by ssr in supabase (new features)

--- app/dashboard/productos/page.tsx
+++ app/dashboard/productos/page.tsx
@@ -1,15 +1,13 @@
 import Products from "@/app/ui/Products";
-import { createClient } from "@supabase/supabase-js";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { createSupabaseServerClient } from "@/app/lib/supabaseServer";
 
 export default async function ProductsPage() {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = await createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("categories")
     .select("id, name")
     .order("created_at", { ascending: false });
 
-  return <Products categories={data ?? []} products={[]} />;
+  return <Products products={[]} categories={data ?? []} />;
 }
