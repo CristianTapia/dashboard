475b71779a72c2d8113d2d6e91df251f7f2362ba
Cristian <ctapia.macaya@gmail.com>
2025-10-28 12:11:58 -0300

    Changing words for testing

--- app/ui/CategoriesClient.tsx
+++ app/ui/CategoriesClient.tsx
@@ -85,7 +85,7 @@ export default function CategoriesPage({ categories }: { categories: Category[] 
                       optionB={
                         <div className="flex gap-2 align-middle">
                           <Trash size={14} />
-                          <span>Eliminara</span>
+                          <span>Eliminar</span>
                         </div>
                       }
                     />
